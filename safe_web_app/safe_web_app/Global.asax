3d8edd4a03f28b5a7376efa8620fa49b0685c239
Stuart Green <stuart@terasmediaco.com>
2019-03-24 15:03:43 -0400

    Initial app created and connected to database with users

---
+++ safe_web_app/safe_web_app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="safe_web_app.MvcApplication" Language="C#" %>
---
+++ safe_web_app/safe_web_app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(safe_web_app.Startup))]
+namespace safe_web_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
